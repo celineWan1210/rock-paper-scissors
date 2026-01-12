c9d52086ee20ad9ab7780b00b59337749e597b6e
YingXuan Wan <wanyingxuan1210@gmail.com>
2026-01-12 08:28:10 +0800

    Update script.js for playGame function logic and added this new file and link it to html file

---
+++ script.js
@@ -0,0 +1,112 @@
+// function 1: get computer choice
+// get computer choice which will return rock, paper or scissors    // use math random to get a number from 0 - 2
+// match with case with 0 -> rock, 1 -> paper, 2 -> scissors
+// return it
+function getComputerChoice() {
+    // return random number from 0 - 2
+    const number = Math.floor(Math.random() * 3);
+        
+    // match case to return the string
+    switch (number) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            break;
+    }
+}
+    
+// function 2: get human choice
+// prompt for huamne choice where user will always return a valid input
+// return it
+function getHumanChoice() {
+    const humanChoice = prompt("Enter rock, paper or scissors");
+    return humanChoice;
+}
+
+// decalre player score and conputer score
+// both new variable is set two 0
+let humanScore = 0;
+let computerScore = 0;
+
+// write logic to paly a single round
+// function play round where it will ask for computer and human choice
+// human choice is case insensitive (always return rock)
+// if rock = rock / paper = paper / scissors = scissors (draw)
+// else if C:rock & H:paper (You win! Paper beats Rock) (humanScore += 1)
+// else if C:paper & H:rock (You lose! Paper beats Rock) (computerScore += 1)
+// else if C:scissors & H:rock (You win! Rock beats scissors) (humanScore += 1)
+// else if C:rock & H:scissors (You lose! Rock beats scissors) (conputerScore += 1)
+// else if C:paper & H:scissors (You win! Scissors beat paper) (humanScore += 1)
+// else if C:scissors & C: paper (You lose! Scissors beat paper) (computerScore += 1)
+function playRound(humanChoice, computerChoice)
+{
+    // case insensitive
+    humanChoice = humanChoice.toLowerCase();
+
+    if (computerChoice == humanChoice) 
+    {
+        console.log("Draw");
+    } else if (computerChoice == "rock" && humanChoice == "paper")
+    {
+        console.log("You win! Paper beats Rock");
+        humanScore += 1;
+    } else if (computerChoice == "paper" && humanChoice == "rock")
+    {
+        console.log("You lose! Paper beats Rock");
+        computerScore += 1; 
+    } else if (computerChoice == "scissors" && humanChoice == "rock")
+    {
+        console.log("You win! Rock beats scissors");
+        humanScore +=1;
+    } else if (computerChoice == "rock" && humanChoice == "scissors")
+    {
+        console.log("You lose! Rock beats scissors");
+        computerScore += 1;
+    } else if (computerChoice == "paper" && humanChoice == "scissors")
+    {
+    console.log("You win! Scissors beat paper");
+        humanScore += 1;
+    } else if (computerChoice == "scissors" && humanChoice == "paper")
+    {
+        console.log("You lose! Scissors beat paper");
+        computerScore += 1;
+    } else 
+    {
+        console.log("Invalid input. You lose");
+        computerScore += 1;
+    }
+}
+
+// add playGame function that call play round five times
+
+
+function playGame() 
+{
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+            
+        playRound(humanSelection, computerSelection);
+    }
+
+    // print score
+    console.log(`Computer Score: ${computerScore}: Human Score: ${humanScore}`)
+    if (computerScore > humanScore)
+    {
+        console.log("You lose!");
+    } else if (humanScore > computerScore)
+    {
+        console.log("You win!");
+    } else 
+    {
+        console.log("Draw");
+    }
+
+}
+
+playGame();
