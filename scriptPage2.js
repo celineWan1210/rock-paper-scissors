61f5a457db5daa18aea737c66e3add9b7ac5fd73
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-14 13:52:15 +0800

    update new reaload function

--- scriptPage2.js
+++ scriptPage2.js
@@ -2,5 +2,5 @@
 const replayButton = document.querySelector(".replay");
 
 replayButton.addEventListener("click", ()=>{
-    window.location.href="index.html";
+    window.location.href="index.html?reload=" + Date.now();
 });
